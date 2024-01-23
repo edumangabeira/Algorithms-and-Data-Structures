07bc71e588e8513bfa040bd2e070cfc4c113404d
edu <edu.mangaba@gmail.com>
2024-01-23 08:49:12 -0300

    feat: insertion_sort

---
+++ sorting/insertion_sort/README.md
@@ -0,0 +1,6 @@
+## Insertion sort
+
+Insertion sort has complexity O(n^2). https://ocw.mit.edu/courses/6-006-introduction-to-algorithms-fall-2011/resources/mit6_006f11_lec03/
+
+
+For the functional approach a simple recursion was enough.
---
+++ sorting/insertion_sort/insertion_sort_functional.py
@@ -0,0 +1,25 @@
+def sort_list(unsorted_list, j):
+    if j < len(unsorted_list):
+        j = j + 1
+        sort_sublist(unsorted_list, 1)
+        sort_list(unsorted_list, j)
+    sorted_list = unsorted_list
+    return sorted_list
+
+def sort_sublist(unsorted_list, i):
+    if i < len(unsorted_list):
+        if unsorted_list[i-1] > unsorted_list[i]:
+                    aux = unsorted_list[i]
+                    unsorted_list[i] = unsorted_list[i-1]
+                    unsorted_list[i-1] = aux
+        i = i + 1
+        sort_sublist(unsorted_list, i)            
+
+unsorted_list = [5, 7, 8, 2, 4]
+print(unsorted_list)
+print(sort_list(unsorted_list, 1))
+
+
+
+
+
